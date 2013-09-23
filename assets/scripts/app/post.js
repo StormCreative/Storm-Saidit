ab55658ae720983c538f09fc816e3d1a1e4e15ed
Ashley Banks <ashleysmbanks89@gmail.com>
2013-09-23 18:07:45 +0100

    all up there

--- assets/scripts/app/post.js
+++ assets/scripts/app/post.js
@@ -1,8 +1,52 @@
+/*
 requirejs.config({
     paths: {
+        Backbone: '../utils/backbone',
         jquery: '../utils/jquery',
         jqueryui: '../utils/jqueryui'
+    },
+    shim: {
+        'Backbone': {
+            deps: ['../utils/lodash', 'jquery'], // load dependencies
+            exports: 'Backbone' // use the global 'Backbone' as the module value
+        }
     }
 });
 
 require(['tagger', 'menu']);
+*/
+
+requirejs.config({
+    paths: {
+        Backbone: '../utils/backbone',
+        jquery: '../utils/jquery'
+    },
+    shim: {
+        'Backbone': {
+            deps: ['../utils/lodash', 'jquery'], // load dependencies
+            exports: 'Backbone' // use the global 'Backbone' as the module value
+        }
+    }
+});
+
+require(['../views/scroller', 'menu'], function(Scroller) {
+    
+    $(".js-back").on('click', function(e) {
+        window.history.go(-1);
+
+        e.preventDefault();
+    });
+
+    if(window.scroll == 1 ) {
+        //var scroller = new Scroller();    
+
+        require(['rating']);
+    }
+    
+    require(['rating']);
+
+    require(['posters']);
+
+    require(['posts']);
+    
+});
